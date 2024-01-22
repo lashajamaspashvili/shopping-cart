55afac0f6b1e10c8fa630fe9ea4503b6dfe74aa2
lashajamaspashvili <ljamaspashvili@gmail.com>
2024-01-22 23:03:42 +0400

    packages, structure and css reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import GlobalCSS from "./assets/styles/global.styled";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <GlobalCSS />
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
